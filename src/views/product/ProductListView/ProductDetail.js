729e69ad1724cf5db63f93a8c236b6ebb5dd65ec
Yuldoshev ahror <yuldashov.axror99@gmail.com>
2020-10-26 15:29:43 +0300

    message ok

--- src/views/product/ProductListView/ProductDetail.js
+++ src/views/product/ProductListView/ProductDetail.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import {
@@ -17,6 +17,9 @@ import {
   InputLabel,
   FormHelperText
 } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Skeleton from '@material-ui/lab/Skeleton';
+import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
   formControl: {
@@ -28,13 +31,93 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const variants = ['h1', 'h3', 'body1', 'caption'];
+
+function TypographyDemo(props) {
+  const { loading = false } = props;
+
+  return (
+    <div>
+      {variants.map((variant) => (
+        <Typography component="div" key={variant} variant={variant}>
+          {loading ? <Skeleton /> : variant}
+        </Typography>
+      ))}
+    </div>
+  );
+}
+
+TypographyDemo.propTypes = {
+  loading: PropTypes.bool,
+};
+
+const myMassArry = [];
+
 const ProductDetail = ({ className, ...rest }) => {
+
+  const [isMyBool, setIsMyBool] = useState(false);
+  const [myUsers, setMyUsers] = useState(false);
+
+  useEffect(() => {
+    let token = localStorage.getItem('logen-authorization');
+    if(token === null)
+      token = '';
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmOTQ3ZjQ5NmYxZjFlNzBiNGU4YmE2NSIsImlhdCI6MTYwMzcwOTQ1MywiZXhwIjoxNjA2MzAxNDUzfQ.40ca0I8Ii83-9chdXOaIvRDhty3XM6J6aeFgFQ-7pyc";
+    axios({
+      method: 'get',
+      url: 'http://195.158.2.207/api/v1/users',
+      headers: {
+        Authorization: 'Bearer ' + token
+      },
+    })
+    .then(result => {
+      for(let i=0; i<result.data.data.length; i++) {
+        if(!myMassArry.includes(result.data.data[i].baggageMass))
+          myMassArry.push(result.data.data[i].baggageMass)
+      }
+      setMyUsers(result.data.data);
+      myMassArry.sort(function(a, b) {
+        return a - b;
+      });
+      console.log(myMassArry);
+    })
+    .catch(err => console.log(err))
+  }, [])
+
   const classes = useStyles();
   const [values, setValues] = useState({
-    from: '',
-    to: '',
+    weight: '',
     message: ''
   });
+
+  const hundlClik = () => {
+    setIsMyBool(true);
+    let token = localStorage.getItem('logen-authorization');
+    if(token === null)
+      token = '';
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmOTQ3ZjQ5NmYxZjFlNzBiNGU4YmE2NSIsImlhdCI6MTYwMzcwOTQ1MywiZXhwIjoxNjA2MzAxNDUzfQ.40ca0I8Ii83-9chdXOaIvRDhty3XM6J6aeFgFQ-7pyc";
+
+    axios({
+      method: 'post',
+      url: 'http://195.158.2.207/api/v1/users/sendmessage',
+      data: {
+        baggageMass: `${values.weight}`,
+        textMessage: values.message,
+      },
+      headers: {
+        Authorization: 'Bearer ' + token
+      },
+    })
+    .then(result => {
+      if(result.data.success)
+        alert('Xabar yuborildi')
+      else
+        alert('Xabar yuborishda xato yuz berdi')
+
+      setIsMyBool(false)
+    })
+    .catch(err => console.log(err))
+  }
 
   const handleChange = event => {
     setValues({
@@ -44,7 +127,10 @@ const ProductDetail = ({ className, ...rest }) => {
   };
 
   return (
-    <form noValidate className={clsx(classes.root, className)} {...rest}>
+
+    
+      myUsers
+      ?<form noValidate className={clsx(classes.root, className)} {...rest}>
       <Card>
         <CardHeader subheader="Haydovchilarga ma'lumot junatish" title="SMS " />
         <Divider />
@@ -58,47 +144,24 @@ const ProductDetail = ({ className, ...rest }) => {
                 <Select
                   labelId="demo-simple-select-outlined-label"
                   id="demo-simple-select-outlined"
-                  value={values.from}
+                  value={values.weight}
                   onChange={handleChange}
                   label="Age"
-                  name="from"
+                  name="weight"
                 >
                   <MenuItem value="" disabled>
                     Tonna
                   </MenuItem>
-                  <MenuItem value={10}>10</MenuItem>
-                  <MenuItem value={20}>20</MenuItem>
-                  <MenuItem value={30}>30</MenuItem>
+                  {myMassArry.map((item) => <MenuItem key={item} value={item}>{item}</MenuItem>)}
+                  <MenuItem value={'ALL'}>ALL</MenuItem>
                 </Select>
                 <FormHelperText>
                   {' '}
-                  Boshlang'ich og'irlikni kiriting{' '}
+                  Og'irlikni kiriting{' '}
                 </FormHelperText>
               </FormControl>
             </Grid>
-            <Grid item md={12} xs={12}>
-              <FormControl variant="outlined" className={classes.formControl}>
-                <InputLabel id="demo-simple-select-outlined-label">
-                  Gacha
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-outlined-label"
-                  id="demo-simple-select-outlined"
-                  value={values.to}
-                  onChange={handleChange}
-                  label="Age"
-                  name="to"
-                >
-                  <MenuItem value="" disabled>
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>10</MenuItem>
-                  <MenuItem value={20}>20</MenuItem>
-                  <MenuItem value={30}>30</MenuItem>
-                </Select>
-                <FormHelperText> Oxirgi og'irlikni kiriting </FormHelperText>
-              </FormControl>
-            </Grid>
+            
             <Grid item md={12} xs={12}>
               <TextField
                 helperText="Xabar kiriting"
@@ -117,12 +180,19 @@ const ProductDetail = ({ className, ...rest }) => {
         </CardContent>
         <Divider />
         <Box display="flex" justifyContent="flex-end" p={2}>
-          <Button color="primary" variant="contained">
+          <Button disabled={isMyBool} onClick={hundlClik} color="primary" variant="contained">
             Send Message
           </Button>
         </Box>
       </Card>
     </form>
+    // scleton
+    :<Grid container spacing={8}>
+        <Grid item xs>
+          <TypographyDemo loading />
+        </Grid>
+      </Grid>
+    
   );
 };
 

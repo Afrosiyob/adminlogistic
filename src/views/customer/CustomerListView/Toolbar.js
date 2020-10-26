a0136b449cd6f32b8c1003fe13e16097b5e37bde
Afrosiyob <afrosiyob.a97@gmail.com>
2020-10-26 15:06:33 +0500

    add
    add

--- src/views/customer/CustomerListView/Toolbar.js
+++ src/views/customer/CustomerListView/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
+import './ToolBar.scss';
 import {
   Box,
   Button,
@@ -9,8 +10,13 @@ import {
   TextField,
   InputAdornment,
   SvgIcon,
-  makeStyles
+  makeStyles,
+  Select,
+  FormControl,
+  MenuItem,
+  InputLabel
 } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { Search as SearchIcon } from 'react-feather';
 import { Link } from 'react-router-dom';
 
@@ -21,11 +27,22 @@ const useStyles = makeStyles(theme => ({
   },
   exportButton: {
     marginRight: theme.spacing(1)
+  },
+  formControl: {
+    minWidth: 200
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
   }
 }));
 
 const Toolbar = ({ className, ...rest }) => {
   const classes = useStyles();
+  const [age, setAge] = React.useState('');
+
+  const handleChange = event => {
+    setAge(event.target.value);
+  };
 
   return (
     <div className={clsx(classes.root, className)} {...rest}>
@@ -39,22 +56,105 @@ const Toolbar = ({ className, ...rest }) => {
       <Box mt={3}>
         <Card>
           <CardContent>
-            <Box maxWidth={500}>
-              <TextField
-                fullWidth
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <SvgIcon fontSize="small" color="action">
-                        <SearchIcon />
-                      </SvgIcon>
-                    </InputAdornment>
-                  )
-                }}
-                placeholder="Search customer"
-                variant="outlined"
-              />
-            </Box>
+            <div className="top-table-wrap">
+              <div className="item-left">
+                <Box maxWidth={500}>
+                  <TextField
+                    className={classes.textField}
+                    fullWidth
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <SvgIcon fontSize="small" color="action">
+                            <SearchIcon />
+                          </SvgIcon>
+                        </InputAdornment>
+                      )
+                    }}
+                    placeholder="Search customer"
+                    variant="outlined"
+                  />
+                </Box>
+              </div>
+              <div className="item-right">
+                <div className="item">
+                  <FormControl
+                    variant="outlined"
+                    className={classes.formControl}
+                  >
+                    <InputLabel id="demo-simple-select-outlined-label">
+                      Locatsiya
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-outlined-label"
+                      id="demo-simple-select-outlined"
+                      value={age}
+                      onChange={handleChange}
+                      label="Age"
+                    >
+                      <MenuItem value="">
+                        <em>None</em>
+                      </MenuItem>
+                      <MenuItem value={10}>
+                        Tewefwefwefwefcwefcwefcewfcwefewn
+                      </MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+                <div className="item">
+                  <FormControl
+                    variant="outlined"
+                    className={classes.formControl}
+                  >
+                    <InputLabel id="demo-simple-select-outlined-label">
+                      Transport type
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-outlined-label"
+                      id="demo-simple-select-outlined"
+                      value={age}
+                      onChange={handleChange}
+                      label="Age"
+                    >
+                      <MenuItem value="">
+                        <em>None</em>
+                      </MenuItem>
+                      <MenuItem value={10}>
+                        Tewefwefwefwefcwefcwefcewfcwefewn
+                      </MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+                <div className="item">
+                  <TextField
+                    id="outlined-basic"
+                    label="Massa (kub)"
+                    variant="outlined"
+                  />
+                </div>
+                <div className="item">
+                  <TextField
+                    id="outlined-basic"
+                    label="Massa (tonna) "
+                    variant="outlined"
+                  />
+                </div>
+                <div className="item">
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    className={classes.button}
+                    startIcon={<DeleteIcon />}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </div>
+            </div>
           </CardContent>
         </Card>
       </Box>
